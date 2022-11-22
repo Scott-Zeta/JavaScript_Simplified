e5c0616b7444404abbc28d441d9d564aed701f05
Scott-Zeta <wc070211@gmail.com>
2022-11-22 17:45:47 +1030

    object basic

--- advance_variables/script.js
+++ advance_variables/script.js
@@ -1,16 +1,24 @@
 const letters = ["a", "b", "c", "d", "e"];
 
-console.log(letters[parseInt(letters.length/2)])
+console.log(letters[parseInt(letters.length / 2)]);
 
 const car = {
-    Make: "Steel",
-    Model: "MK4",
-    isUsed: true,
-    makeNoise: () => console.log("Beep~~!"),
-    makeNoise2 (){
-        console.log("No arrow Beep!")
-    }
-}
+  Make: "Steel",
+  Model: "MK4",
+  isUsed: true,
+  makeNoise: () => console.log("Beep~~!"),
+  makeNoise2() {
+    console.log("No arrow Beep!");
+  },
+};
 
-car.makeNoise()
-car.makeNoise2()
+car.makeNoise();
+car.makeNoise2();
+
+const book = {
+  title: "Eat my Shit",
+  author: {
+    name: "Scott Zeta",
+    age: 99,
+  },
+};
