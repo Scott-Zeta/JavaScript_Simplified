b15d4ff21213e2344674005aa67027629ce68cb2
Scott-Zeta <wc070211@gmail.com>
2022-11-23 17:27:32 +1030

    confusing reduce method notes

--- advance_variables/README.md
+++ advance_variables/README.md
@@ -3,3 +3,6 @@
 2. specaial case. "===" will campare null and undefined as false since they are different type. 
 3. for not equal, use "!=="
 4. NaN, not number isn't equal to anyhing, if want to check, there is a build-in function isNaN()
+5. map doesn't modify old array but create a new one
+## 6. very tricky fact, the parameters in array method order is fixed, such as reduce, first one can only be accumulator(previous value), second can only be cuurent iterating elements
+# I HATE THIS!
---
+++ advance_variables/arrayMethod.js
@@ -0,0 +1,40 @@
+const a = [1, 2, 4, 5];
+
+//a.forEach((n) => console.log(n));
+
+//a.forEach((index, number, x, y, z) => console.log(index, number, x, y, z));
+
+//map
+let b = a.map((n) => n * 2);
+
+b = a.map((n) => {
+  return n + 1;
+});
+//console.log(b);
+
+//filter
+const c = b.filter((n) => n % 2 == 0);
+//console.log(c);
+
+//find return the first one found, or undefined
+let n = c.find((n) => n > 2);
+//console.log(n);
+
+//some almost the same, but return boolean, seek for if there is any
+n = c.some((n) => n > 2);
+//console.log(n);
+
+//every check every element
+n = c.every((n) => n > 2);
+
+//reduce, return all elements in array into one
+const sum = a.reduce((sum, n) => {
+  return sum + n*2;  //1,2,4,5      ((((0*2 + 1)*2 + 2)*2 + 3)*2 + 4)*2 + 5 = 29
+}, 0);
+console.log(sum)
+const sum2 = a.reduce((sum, n) => {
+    return sum*2 + n; //1,2,4,5     1*2+2*2+4*2+5*2 = 24
+  }, 0);
+console.log(sum2)
+
+a.reduce()
