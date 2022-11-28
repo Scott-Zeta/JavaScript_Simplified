ef7e7708c394397f7a34d30d83cfb123d09651fb
Scott-Zeta <wc070211@gmail.com>
2022-11-28 17:11:53 +1030

    basic event listener

--- basic_dom/script.js
+++ basic_dom/script.js
@@ -10,10 +10,26 @@ IDdiv.style.color = "red";
 let Classdiv = document.getElementsByClassName("div-class");
 console.log(Classdiv);
 
-const ClassDivArray = Array.from(Classdiv) //need to convert into a true array
-ClassDivArray.forEach(e => (e.style.color="blue"))
-
+const ClassDivArray = Array.from(Classdiv); //need to convert into a true array
+ClassDivArray.forEach((e) => (e.style.color = "blue"));
 
 //it will return an array(an collection) of elements with same class name
 
 //different elements can have same class name
+
+const btn = document.querySelector("[data-btn]");
+console.log(btn);
+
+//event listener, first parameter behaviour, second the function want to excute
+btn.addEventListener("click", () => console.log("clicked"));
+const clicked2 = () => console.log("clicked 2");
+btn.addEventListener("click", clicked2);
+
+//remove eventlistener(can not use anoymous)
+btn.removeEventListener("click", clicked2);
+
+const inputbox = document.querySelector('[data-text]')
+
+inputbox.addEventListener('input', e => {
+    console.log(e.target.value === '')
+})
