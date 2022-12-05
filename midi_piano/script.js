68754dc8183553e369ef61d16af5b33dd8178667
Scott-Zeta <wc070211@gmail.com>
2022-12-05 11:55:52 +1030

    finished the piano through tutorial

--- midi_piano/script.js
+++ midi_piano/script.js
@@ -1,22 +1,35 @@
+const audioContext = new AudioContext()
+
 const NOTE_DETAILS = [
-  { note: "C", key: "Z", frequency: 261.626, active: false },
-  { note: "Db", key: "S", frequency: 277.183, active: false },
-  { note: "D", key: "X", frequency: 293.665, active: false },
-  { note: "Eb", key: "D", frequency: 311.127, active: false },
-  { note: "E", key: "C", frequency: 329.628, active: false },
-  { note: "F", key: "V", frequency: 349.228, active: false },
-  { note: "Gb", key: "G", frequency: 369.994, active: false },
-  { note: "G", key: "B", frequency: 391.995, active: false },
-  { note: "Ab", key: "H", frequency: 415.305, active: false },
-  { note: "A", key: "N", frequency: 440, active: false },
-  { note: "Bb", key: "J", frequency: 466.164, active: false },
-  { note: "B", key: "M", frequency: 493.883, active: false },
+  { note: "C", key: "Z", frequency: 261.626 },
+  { note: "Db", key: "S", frequency: 277.183 },
+  { note: "D", key: "X", frequency: 293.665 },
+  { note: "Eb", key: "D", frequency: 311.127 },
+  { note: "E", key: "C", frequency: 329.628 },
+  { note: "F", key: "V", frequency: 349.228 },
+  { note: "Gb", key: "G", frequency: 369.994 },
+  { note: "G", key: "B", frequency: 391.995 },
+  { note: "Ab", key: "H", frequency: 415.305 },
+  { note: "A", key: "N", frequency: 440 },
+  { note: "Bb", key: "J", frequency: 466.164 },
+  { note: "B", key: "M", frequency: 493.883 },
 ];
 const play = () => {
   NOTE_DETAILS.forEach((n) => {
     const keyElement = document.querySelector(`[data-note="${n.note}"]`);
-    keyElement.classList.toggle("active", n.active);
+    keyElement.classList.toggle("active", n.active || false);
+    if(n.oscillator != null){
+      n.oscillator.disconnect()
+    }
   });
+
+  //this allow multiple keys in active return a set
+  const currentActive = NOTE_DETAILS.filter(n => n.active)
+  console.log(currentActive)
+  const gain = 1/currentActive.length
+  currentActive.forEach(n =>{
+    startNote(n,gain)
+  })
 };
 
 const getNote = (keyBoardOperation) => {
@@ -45,3 +58,15 @@ document.addEventListener("keyup", (e) => {
   note.active = false;
   play()
 });
+
+function startNote(n,gain){
+  const gainNode = audioContext.createGain()
+  gainNode.gain.value = gain
+  const oscillator = audioContext.createOscillator()
+  oscillator.frequency.value = n.frequency
+  oscillator.type = "sine"
+  //connect oscillator to audio context
+  oscillator.connect(gainNode).connect(audioContext.destination)
+  oscillator.start()
+  n.oscillator = oscillator
+}
