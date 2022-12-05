25a5a59d6808840d040286f4a933474f5ad8e939
Scott-Zeta <wc070211@gmail.com>
2022-12-05 14:54:26 +1030

    finished

--- quiz/script.js
+++ quiz/script.js
@@ -5,8 +5,50 @@
     * BONUS: The questions (have the class `question-item`)
     * BONUS: The alert (has the id `alert`)
 */
+const form = document.querySelector("#quiz-form");
+const answers = Array.from(document.querySelectorAll(".answer")); //no filter for node list, must turn into array
+const question = Array.from(document.querySelectorAll(".question-item"));
+const alter = document.querySelector("#alert");
+console.log(form);
+console.log(answers);
+console.log(question);
+console.log(alter);
 
 // TODO: 3. Create a submit event listener for the form that does the following.
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // answers.forEach(e => console.log(e.checked))
+  //get the all checked anwser
+  answers.forEach((e) =>
+    e.closest(".question-item").classList.add("incorrect")
+  ); //marked all question not anwsered as incorrect
+  const checkedAnwsers = answers.filter((a) => a.checked);
+  console.log(checkedAnwsers);
+  //check the selected value is true or not
+  checkedAnwsers.forEach((e) => {
+    console.log(e.value === "true");
+    const question = e.closest(".question-item"); //find the question it belongs to
+    if (e.value === "true") {
+      question.classList.remove("incorrect");
+      question.classList.add("correct");
+    } else {
+      question.classList.remove("incorrect");
+      question.classList.add("incorrect");
+    }
+  });
+  const checkAllCorrect = question.every((e) =>
+    e.classList.contains("correct")
+  );
+  if (checkAllCorrect) {
+    console.log("All correct");
+    alter.classList.add("active");
+    setTimeout(() => {
+      alter.classList.remove("active");
+    }, 2000);
+  } else {
+    console.log("Not All");
+  }
+});
 //    1. Prevent the default behaviour
 //    2. Get all selected answers (use the `checked` property on the input to determine if it is selected or not)
 //    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
