4c7ebecf6702025b5960c59278fa285b8241e0dd
Scott-Zeta <wc070211@gmail.com>
2022-12-12 18:43:53 +1030

    error message show up

--- form_validation/script.js
+++ form_validation/script.js
@@ -9,6 +9,40 @@
 //      3. Ensure the password and confirmation password match
 //      4. Ensure the terms checkbox is checked
 //    TODO: If there are any errors then prevent the form from submitting and show the error messages
+const form = document.querySelector("#form")
+const usernameInput = document.querySelector("#username")
+const passwordInput = document.querySelector("#password")
+const passwordRepeat = document.querySelector("#password-confirmation")
+const termConfirmation = document.querySelector("#terms")
+const errorBox = document.querySelector(".errors")
+const errorList = document.querySelector(".errors-list")
+
+form.addEventListener("submit", e =>{
+    const list = []
+    if(usernameInput.value.length < 6){
+        console.log("short username")
+        list.push("short username")
+    }
+    if(passwordInput.value.length < 10){
+        console.log("short password")
+        list.push("short password")
+    }
+    if(passwordInput.value != passwordRepeat.value){
+        console.log("password must be same")
+        list.push("password must be same")
+    }
+    if(!termConfirmation.checked){
+        console.log("term not checked")
+        list.push("term not checkek")
+    }
+
+    if(list.length>0){
+        e.preventDefault()
+        showErrors(list)
+    }
+})
+
+
 
 // TODO: Define this function
 function clearErrors() {
@@ -17,6 +51,7 @@ function clearErrors() {
     // I recommend using a while loop to accomplish this task
     // This is the trickiest part of this exercise so if you get stuck and are unable to progress you can also set the innerHTML property of the error-list to an empty string and that will also clear the children. I recommend trying to accomplish this with a while loop, though, for practice.
     // Also, make sure you remove the show class to the errors container
+    
   }
   
   // TODO: Define this function
@@ -24,4 +59,10 @@ function clearErrors() {
     // Add each error to the error-list element
     // Make sure to use an li as the element for each error
     // Also, make sure you add the show class to the errors container
+    errorMessages.forEach(e => {
+        const li = document.createElement("li")
+        li.innerText = e
+        errorList.append(li)
+    })
+    errorBox.classList.add("show")
   }
