799478acecc3e9017fb5ce3df854e9a7a9f4b803
Scott-Zeta <wc070211@gmail.com>
2023-02-28 12:18:55 +1030

    response method

--- Ajax_and_Fetch/Ajax_script.js
+++ Ajax_and_Fetch/Ajax_script.js
@@ -2,15 +2,32 @@
 
 const url = "https://developer.nps.gov/api/v1/parks?stateCode=ca&api_key=39SBa6EygP38f6E30TfpuL3daSd2xu4FpgVoe0LQ"
 
+const updateUISuccess = (data) =>{
+    console.log(data)
+}
+
+const updateError = (error) =>{
+    console.error(error)
+}
+
+const responseMethod = function(httpRequest) {
+    //respnse only works when get something
+    if(httpRequest.readyState === 4){
+        if(httpRequest.status ===200){
+            updateUISuccess(httpRequest.responseText)
+        }else{
+            updateError(httpRequest.status+": "+ httpRequest.responseText)
+        }
+    }else{
+        console.error("ReadyState: " + httpRequest.readyState)
+    }
+}
+
 const createRequest = function(url){
     //1. create the request
     const httpRequest = new XMLHttpRequest(url)
-    httpRequest.addEventListener('readystatechange',(url)=>{
-        //when readyState is 4, means get the response(possible success or error)
-        if(httpRequest.readyState == 4){
-            console.log(httpRequest.responseText)
-        }
-    })
+    //4. set the eventListener method to deal with response when readystate changed
+    httpRequest.addEventListener('readystatechange', () => responseMethod(httpRequest))
     //2. open the request
     httpRequest.open('GET',url)
     //3. send the request
